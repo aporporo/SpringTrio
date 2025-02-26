ef4c20ff10a64cd863b3caa35d20d3f1f374bb65
aporporo <117672857+aporporo@users.noreply.github.com>
2025-02-26 16:31:25 +0300

    Update WebsocketConfiguration.java
    Added comments

--- src/main/java/com/example/otrio/config/WebsocketConfiguration.java
+++ src/main/java/com/example/otrio/config/WebsocketConfiguration.java
@@ -9,12 +9,20 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebsocketConfiguration implements WebSocketMessageBrokerConfigurer {
+    /*
+    * Registers websocket endpoint "/game/gameMove" for client connections
+    * Uses SockJS when WebSockets not available
+    */
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/game/gameMove").withSockJS();
 
     }
 
+    /*
+    Clients send messages to "/app", which will be handled by controllers
+    Clients can subscribe "/topic" to receive real-time updates
+     */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app").enableSimpleBroker("/topic");
